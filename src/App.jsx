427110cd363a5dcef5dec9948ff49c431050e906
SymuNadeem <msymunnadeem@gmail.com>
2024-07-01 12:25:56 +0600

    upload client-side

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "../src/style/index.css";
+
+function App() {
+  return (
+    <>
+      <div className=" text-red-700 font-bold text-3xl">Hello CRUD</div>
+    </>
+  );
+}
+
+export default App;
